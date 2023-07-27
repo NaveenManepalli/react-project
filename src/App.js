901c346f8540c3d944ecc9dc3b076952f35df623
Naveen.B <86225990+NaveenManepalli@users.noreply.github.com>
2023-07-27 14:32:30 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Header';
+import Counter1 from './Counter1';
+
+function App() {
+  return (
+    <div className="App">
+     <Header title="rajalakshmi institute of technology"/>
+     <Header title="Welcome"/>
+     <Counter1 title="Counter"/>
+    </div>
+   
+  );
+}
+
+export default App;
